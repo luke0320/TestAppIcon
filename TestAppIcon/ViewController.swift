283c55644d8f13329cea49d03394823184e35105
Luke <lukelee011@outlook.com>
2021-07-14 16:28:17 +0800

    add icon selection status

--- TestAppIcon/ViewController.swift
+++ TestAppIcon/ViewController.swift
@@ -18,6 +18,33 @@ class ViewController: UIViewController {
             case .alternative: return "Icon-2"
             }
         }
+        
+        init(iconName: String?) {
+            self = AppIcon.allCases.first{$0.iconName == iconName} ?? .default
+        }
+    }
+    
+    private var label: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Select Your App Icon"
+        lbl.font = .systemFont(ofSize: 20, weight: .bold)
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.textAlignment = .center
+        return lbl
+    }()
+    
+    private var iconContainer: UIStackView = {
+        let sv = UIStackView()
+        sv.axis = .horizontal
+        sv.alignment = .fill
+        sv.distribution = .fill
+        sv.spacing = 50
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
+    
+    private lazy var appIcons = AppIcon.allCases.map {
+        makeIconButton(imageName: $0.iconName, buttonTag: $0.rawValue)
     }
     
     override func viewDidLoad() {
@@ -25,54 +52,64 @@ class ViewController: UIViewController {
         setupView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateIconButtonSelection()
+    }
+    
     private func setupView() {
         
-        let lbl = UILabel()
-        lbl.text = "Select Your App Icon"
-        lbl.font = .systemFont(ofSize: 20, weight: .bold)
-        lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.textAlignment = .center
-        
-        let icons = AppIcon.allCases.map {
-            makeIconButton(imageName: $0.iconName, buttonTag: $0.rawValue)
+        appIcons.forEach {
+            iconContainer.addArrangedSubview($0)
         }
         
-        let sv = UIStackView(arrangedSubviews: icons)
-        sv.axis = .horizontal
-        sv.alignment = .fill
-        sv.distribution = .fill
-        sv.spacing = 50
-        sv.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(lbl)
-        view.addSubview(sv)
+        view.addSubview(label)
+        view.addSubview(iconContainer)
         
         NSLayoutConstraint.activate([
-            sv.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            sv.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            lbl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            lbl.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
-            lbl.bottomAnchor.constraint(equalTo: sv.topAnchor, constant: -20)
+            iconContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            iconContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            label.bottomAnchor.constraint(equalTo: iconContainer.topAnchor, constant: -20)
         ])
     }
 
     private func makeIconButton(imageName: String, buttonTag: Int, iconSize: CGFloat = 100) -> UIButton {
         let btn = UIButton()
-        btn.setImage(UIImage(named: imageName), for: .normal)
         btn.tag = buttonTag
-        btn.addTarget(self, action: #selector(onIconButtonTapped), for: .touchUpInside)
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.contentVerticalAlignment = .fill
+        
+        btn.setImage(UIImage(named: imageName), for: .normal)
         btn.contentHorizontalAlignment = .fill
+        btn.contentVerticalAlignment = .fill
+        
+        btn.layer.borderColor = UIColor.orange.cgColor
         btn.layer.cornerRadius = iconSize / 10
         btn.clipsToBounds = true
+        
+        btn.translatesAutoresizingMaskIntoConstraints = false
         btn.widthAnchor.constraint(equalToConstant: iconSize).isActive = true
         btn.heightAnchor.constraint(equalToConstant: iconSize).isActive = true
+        
+        btn.addTarget(self, action: #selector(onIconButtonTapped), for: .touchUpInside)
+        
         return btn
+    }
+    
+    private func updateIconButtonSelection() {
+        let currentIcon = AppIcon(iconName: UIApplication.shared.alternateIconName)
+        appIcons.forEach {
+            let isSelected = currentIcon == AppIcon(rawValue: $0.tag)
+            $0.layer.borderWidth = isSelected ? 4 : 0
+        }
     }
     
     @objc
     private func onIconButtonTapped(_ sender: UIButton) {
+        defer {
+            updateIconButtonSelection()
+        }
+        
         guard
             sender.tag != AppIcon.default.rawValue,
             let appIcon = AppIcon(rawValue: sender.tag)
